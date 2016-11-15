eebe49674d9cb43dc125f94615ac29c581ab46cc
1vonne1 <iibacachev@gmail.com>
2016-11-15 08:31:42 -0400

    Update listas.py

--- listas.py
+++ listas.py
@@ -2,3 +2,16 @@
 #Ivonne Ibacache
 #Rocio Jimenez
 #Maximiliano Toro
+import random
+
+def genera ():
+  l=range [1,42]
+  mi_lista=[]
+  while len (mi_lista)<6:
+    numero=random.choice(l)
+
+if not (numero) in mi_lista:
+  mi_lista.append(numero)
+
+return mi_lista
+
