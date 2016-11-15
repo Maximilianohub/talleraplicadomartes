5fb12d30517017c7fd09c689fc0d7750ba6693f4
Rocio1 <rocio.belen.j@gmail.com>
2016-11-15 08:50:22 -0400

    Update listas.py

--- listas.py
+++ listas.py
@@ -2,19 +2,36 @@
 #Ivonne Ibacache
 #Rocio Jimenez
 #Maximiliano Toro
-import random
-#modificacion
-def genera():
-      lista = [0]  * 6
-      y=0
-      for i in range(6):
-          x=random.randint(1,41)
-          while x==y:
-              x=random.randint(1,41)
-          lista[i] = x
-          y=x
-      lista.sort()
-      print lista
 
+#import random
+#modificacion
+#def genera():
+#     lista = [0]  * 6
+ #    y=0
+#      for i in range(6):
+#          x=random.randint(1,41)
+#          while x==y:
+#              x=random.randint(1,41)
+#          lista[i] = x
+#          y=x
+#      lista.sort()
+#      print lista
+
+#genera()
+
+import random
+
+def genera():
+  l=range(1,42)
+  mi_lista=[]
+  while len(mi_lista)<6:
+    numero=random.choice(l)
+    if not (numero) in mi_lista:
+      mi_lista.append(numero)
+      print mi_lista
 genera()
 
+def comprueba(x,l):
+    if x in l:
+        return False
+  
